e771385ac4d63072f1696c525de9ca2a385a2b01
JaydenElliott <jayden.elliott@outlook.com>
2021-03-05 21:44:43 +1100

    BUILD: middle col chat styling and appended global styles.

--- src/components/chat-bar/chatBar.js
+++ src/components/chat-bar/chatBar.js
@@ -5,7 +5,15 @@ import React from "react";
 import "./chatBar.scss";
 
 function ChatBar(props) {
-  return <div className="chat-bar"></div>;
+  return (
+    <div className="chat-bar">
+      <div className="chat-bar-header"></div>
+      <div className="chat-bar-center"></div>
+      <div className="chat-bar-footer">
+        <div className="chat-bar-footer-input-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatBar;
